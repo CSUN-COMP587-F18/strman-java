4e6aced0a2ecc794aad64ad6ad89dad57e69c151
LuanTa-CSUN <luan.ta.32@my.csun.edu>
2018-10-31 11:52:34 -0700

    Re-uploaded propertyTest.java

--- propertyTest.java
+++ propertyTest.java
@@ -1,27 +1,24 @@
-package propertytest.com.strman;
-
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
-// import com.strman.Strman;
 import org.junit.runner.RunWith;
 
-//import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.endsWith;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeThat;
 import static org.hamcrest.Matchers.containsString;
 
-//import org.junit.Test;
-
-//import static org.hamcrest.CoreMatchers.*;
-
-//import static org.junit.Assert.*;
-import static com.strman.Strman.*;
 
 @RunWith(JUnitQuickcheck.class)
 public class propertyTest {
     @Property
     public void containsReturnTrue(final String value, final String needle) {
         assumeThat(value, containsString(needle));
-        assertTrue(contains(value, needle));
+        assertTrue(Strman.contains(value, needle));
+    }
+
+    @Property
+    public void endsWithReturnTrue(final String value, final String search) {
+        assumeThat(value, endsWith(search));
+        assertTrue(Strman.endsWith(value, search));
     }
 }
